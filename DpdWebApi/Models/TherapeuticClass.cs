73fc1d9b9e2624e0b14e12d78a0f976c72373f27
jihuang <jian.huang@hc-sc.gc.ca>
2026-01-27 11:00:27 -0500

    DPD No longer provide  AHFS info

--- DpdWebApi/Models/TherapeuticClass.cs
+++ DpdWebApi/Models/TherapeuticClass.cs
@@ -11,8 +11,9 @@ namespace DpdWebApi.Models
         public int drug_code { get; set; }
         public string tc_atc_number { get; set; }
         public string tc_atc { get; set; }
-        public string tc_ahfs_number { get; set; }
-        public string tc_ahfs { get; set; }
+        //2026 March, AHFS no longer support in DPD API
+        //public string tc_ahfs_number { get; set; }
+        //public string tc_ahfs { get; set; }
 
     }
 }
